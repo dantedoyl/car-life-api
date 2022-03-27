8c9833dd6f592f9c13eb3cdc09903c34f38f579b
Alekhin Sergey <seralekhin@ozon.ru>
2022-03-27 14:19:28 +0300

    fix

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -4,8 +4,26 @@ import "net/http"
 
 func CorsControlMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Access-Control-Allow-Headers", "content-type")
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:10888")
+
+		switch req.Header.Get("Origin") {
+		case "http://localhost:10888":
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:10888")
+
+		case "http://127.0.0.1:10888":
+			w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:10888")
+
+		case "http://localhost:3000":
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+
+		case "http://127.0.0.1:3000":
+			w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3000")
+
+		case "http://89.208.199.170":
+			w.Header().Set("Access-Control-Allow-Origin", "http://89.208.199.170")
+		}
+
+		w.Header().Set("Access-Control-Allow-Headers", "content-type")
 		w.Header().Set("Access-Control-Expose-Headers", "X-CSRF-Token")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
