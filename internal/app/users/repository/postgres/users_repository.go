3e59146e773846db647ed4a668c8322cf3d50965
dantedoyl <makstorzhkov@yandex.ru>
2022-04-01 23:40:44 +0300

    session with map

--- internal/app/users/repository/postgres/users_repository.go
+++ internal/app/users/repository/postgres/users_repository.go
@@ -2,7 +2,6 @@ package users_repository
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"github.com/dantedoyl/car-life-api/internal/app/models"
 	"github.com/dantedoyl/car-life-api/internal/app/users"
@@ -12,13 +11,15 @@ import (
 
 type UsersRepository struct {
 	sqlConn       *sql.DB
-	tarantoolConn *tarantool.Connection
+	//tarantoolConn *tarantool.Connection
+	userSessions map[string]*models.Session
 }
 
 func NewUserRepository(connP *sql.DB, connT *tarantool.Connection) users.IUsersRepository {
 	return &UsersRepository{
 		sqlConn:       connP,
-		tarantoolConn: connT,
+		//tarantoolConn: connT,
+		userSessions: make(map[string]*models.Session),
 	}
 }
 
@@ -89,61 +90,75 @@ func (ur *UsersRepository) SelectByID(userID uint64) (*models.User, error) {
 }
 
 func (ur *UsersRepository) Insert(session *models.Session) error {
-	data, err := json.Marshal(session)
-	if err != nil {
-		return err
-	}
-
-	dataStr := string(data)
-
-	//resp, err := sr.dbConn.Eval("return new_session(...)", []interface{}{session.Value, dataStr})
-	_, err = ur.tarantoolConn.Insert("sessions", []interface{}{session.Value, dataStr})
-	if err != nil {
-		return err
-	}
+	//data, err := json.Marshal(session)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//dataStr := string(data)
+	//
+	////resp, err := sr.dbConn.Eval("return new_session(...)", []interface{}{session.Value, dataStr})
+	//_, err = ur.tarantoolConn.Insert("sessions", []interface{}{session.Value, dataStr})
+	//if err != nil {
+	//	return err
+	//}
+	//______________________________________
+	// map session
+	ur.userSessions[session.Value] = session
 
 	return nil
 }
 
 func (ur *UsersRepository) SelectByValue(sessValue string) (*models.Session, error) {
-	resp, err := ur.tarantoolConn.Call("check_session", []interface{}{sessValue})
-	if err != nil {
-		return nil, err
-	}
+	//resp, err := ur.tarantoolConn.Call("check_session", []interface{}{sessValue})
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//data := resp.Data[0]
+	//if data == nil {
+	//	return &models.Session{}, nil
+	//}
+	//
+	//sessionDataSlice, ok := data.([]interface{})
+	//if !ok {
+	//	return nil, fmt.Errorf("cannot cast data")
+	//}
+	//
+	//if sessionDataSlice[0] == nil {
+	//	return nil, fmt.Errorf("session not exist")
+	//}
+	//
+	//sessionData, ok := sessionDataSlice[1].(string)
+	//if !ok {
+	//	return nil, fmt.Errorf("cannot cast to string")
+	//}
+	//
+	//sess := &models.Session{}
+	//err = json.Unmarshal([]byte(sessionData), sess)
+	//if err != nil {
+	//	return nil, err
+	//}
 
-	data := resp.Data[0]
-	if data == nil {
-		return &models.Session{}, nil
-	}
-
-	sessionDataSlice, ok := data.([]interface{})
+	//______________________________________
+	// map session
+	sess, ok := ur.userSessions[sessValue]
 	if !ok {
-		return nil, fmt.Errorf("cannot cast data")
-	}
-
-	if sessionDataSlice[0] == nil {
-		return nil, fmt.Errorf("session not exist")
-	}
-
-	sessionData, ok := sessionDataSlice[1].(string)
-	if !ok {
-		return nil, fmt.Errorf("cannot cast to string")
-	}
-
-	sess := &models.Session{}
-	err = json.Unmarshal([]byte(sessionData), sess)
-	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("no session")
 	}
 
 	return sess, nil
 }
 
 func (ur *UsersRepository) DeleteByValue(sessionValue string) error {
-	_, err := ur.tarantoolConn.Delete("sessions", "primary", []interface{}{sessionValue})
-	if err != nil {
-		return err
-	}
+	//_, err := ur.tarantoolConn.Delete("sessions", "primary", []interface{}{sessionValue})
+	//if err != nil {
+	//	return err
+	//}
+
+	//______________________________________
+	// map session
+	delete(ur.userSessions, sessionValue)
 
 	return nil
 }
